0d62f1d1553aaf81cff6cbadd79d03f8ec1986b0
Deuslleyw <Deuslleyw@hotmail.com>
2025-07-15 15:04:46 -0300

    implClassClient

---
+++ src/main/java/com/deusley/api_clienteIA/domain/Cliente.java
@@ -0,0 +1,27 @@
+package com.deusley.api_clienteIA.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "CLIENTE")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String nome;
+    private Integer idade;
+    private Integer compras;
+    private Double ticketMedio;
+    private String Classificacao;
+
+
+}
