76e96863938900aeca910436a180af10b41f9ad0
Deuslleyw <Deuslleyw@hotmail.com>
2025-07-15 14:49:32 -0300

    firstCommit

---
+++ src/main/java/com/deusley/api_clienteIA/ApiClienteIaApplication.java
@@ -0,0 +1,13 @@
+package com.deusley.api_clienteIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClienteIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClienteIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deusley/api_clienteIA/ApiClienteIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusley.api_clienteIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClienteIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
