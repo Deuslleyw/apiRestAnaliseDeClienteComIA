95dee983ab9c6d05f31f8b8c16d3e9dd060872d8
Deuslleyw <Deuslleyw@hotmail.com>
2025-07-21 18:02:42 -0300

    impPromptAndOuthers

---
+++ src/main/java/com/deusley/api_clienteIA/integration/OpenClienteIA.java
@@ -0,0 +1,25 @@
+package com.deusley.api_clienteIA.integration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+public class OpenClienteIA {
+
+    @Value("${open.api.key}")
+    private String apiKey;
+
+    private final WebClient webClient = WebClient.builder()
+            .baseUrl("https://api.openai.com/v1/chat/completions")
+            .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer" + apiKey)
+            .build();
+    public String classificarCliente(String prompt){
+        String requestBody = "{" + "\"model\": \"gpt-3.5-turbo\","
+                                 + "\"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]"
+                                 + "}";
+    }
+}
