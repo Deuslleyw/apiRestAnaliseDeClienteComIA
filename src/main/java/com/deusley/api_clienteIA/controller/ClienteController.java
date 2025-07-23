1663a1ae25b5662dbff318febb439c80e3d20e29
Deuslleyw <Deuslleyw@hotmail.com>
2025-07-23 00:27:12 -0300

    implController

---
+++ src/main/java/com/deusley/api_clienteIA/controller/ClienteController.java
@@ -0,0 +1,15 @@
+package com.deusley.api_clienteIA.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/v1/clientes")
+@RequiredArgsConstructor
+public class ClienteController {
+
+
+
+
+}
