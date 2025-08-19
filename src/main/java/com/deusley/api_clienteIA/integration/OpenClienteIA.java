ca9ca420e3fe6ec75598cec6a006e10650145a30
Deuslleyw <Deuslleyw@hotmail.com>
2025-08-19 18:27:42 -0300

    update

--- src/main/java/com/deusley/api_clienteIA/integration/OpenClienteIA.java
+++ src/main/java/com/deusley/api_clienteIA/integration/OpenClienteIA.java
@@ -1,11 +1,18 @@
 package com.deusley.api_clienteIA.integration;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Component
 @RequiredArgsConstructor
@@ -15,6 +22,7 @@ public class OpenClienteIA {
     private String apiKey;
 
     private WebClient webClient;
+    private Bucket bucket;
 
     @PostConstruct
     public void init() {
@@ -23,9 +31,17 @@ public class OpenClienteIA {
                 .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
                 .build();
+
+
+        Bandwidth limit = Bandwidth.classic(60, Refill.greedy(60, Duration.ofMinutes(1)));
+        this.bucket = Bucket.builder().addLimit(limit).build();
     }
 
     public String classificarCliente(String prompt) {
+        if (!bucket.tryConsume(1)) {
+            throw new RuntimeException("Limite de requisições atingido. Tente novamente mais tarde.");
+        }
+
         String requestBody = """
             {
               "model": "gpt-3.5-turbo",
@@ -38,6 +54,11 @@ public class OpenClienteIA {
                 .bodyValue(requestBody)
                 .retrieve()
                 .bodyToMono(String.class)
+                .retryWhen(
+                        Retry.backoff(3, Duration.ofSeconds(10))
+                                .filter(ex -> ex instanceof WebClientResponseException wex &&
+                                        wex.getStatusCode().value() == 429)
+                )
                 .block();
     }
 }
